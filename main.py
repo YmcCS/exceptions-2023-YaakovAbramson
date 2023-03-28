0785c679a1884f7ef28ad3ca2b1f9fe8957ff482
Yaakov Abramson <114956127+YaakovAbramson@users.noreply.github.com>
2023-03-28 15:50:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ except TypeError:
     print("TypeError")
 except EOFError:
     print("Don't do that. ")
+except ZeroDivisionError:
+    print("Imagine that you have zero cookies and you split them evenly among zero friends, How many cookies does each person get? See? It doesn't make sense. And Cookie Monster is sad that there are no cookies, and you are sad that you have no friends. ")
+except:
+    print("The brown cat jumped over the quick fox after committing suicide on the beach. ")
